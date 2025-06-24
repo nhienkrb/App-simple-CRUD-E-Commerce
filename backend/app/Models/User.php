f69ea7e0ec16c464030b211625580fa6d61f3d30
nhiendev <nhiennkrb@gmail.com>
2025-06-24 23:38:10 +0700

    chore: update the relationship to eager load

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -25,6 +25,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'password',
     ];
 
+    protected $with = ['loyaltyPoints'];
+
     /**
      * The attributes that should be hidden for serialization.
      *
