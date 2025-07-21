d2259907b04233c5e5a41ee0755d4854e990d457
nhiendev <nhiennkrb@gmail.com>
2025-07-21 22:51:25 +0700

    chore: change routers  from en to vn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,12 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import ShoppingCart from "./pages/shopping-cart/ShoppingCart";
 import { CartProvider } from "./context/CartContext";
-import News from "./pages/news/NEws";
+import News from "./pages/news/News";
 import Contact from "./pages/contact/Contact";
 import Footer from "./components/footer/Footer";
-import FloatingSocialButtons from "./components/FloatingSocialButtons";
 import LayoutProductDetail from "./pages/product-detail/LayoutProductDetail";
 import ProductDetail from "./pages/product-detail/ProductDetail";
 import NoPage from "./pages/NoPage";
-import DashboardLayout from "./pages/admin/DashboardLayout";
-import AdminRoutes from "./routes/AdminRoutes";
 import SignIn from "./pages/auth/SignIn";
 import SignUp from "./pages/auth/SignUp";
 import AuthProvider from "./context/AuthContext";
@@ -37,26 +34,25 @@ export default function App() {
             {/* Công khai */}
             <Route path="/login" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="cart" element={<ShoppingCart />} />
-                <Route path="admin/*" element={<AdminRoutes />} />
-              </Route>
+            <Route element={<ProtectedRoute />}>
+              <Route path="cart" element={<ShoppingCart />} />
+            </Route>
             {/* Layout chung: Navbar + Footer */}
             <Route path="/" element={<MainLayout />}>
               {/* Public routes */}
               <Route index element={<Home />} />
-              <Route path="home" element={<Home />} />
-              <Route path="products" element={<Products />} />
-              <Route path="news" element={<News />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="product" element={<LayoutProductDetail />}>
+              <Route path="trang-chu" element={<Home />} />
+              <Route path="san-pham" element={<Products />} />
+              <Route path="tin-tuc" element={<News />} />
+              <Route path="lien-he" element={<Contact />} />
+              <Route path="gio-hang" element={<ShoppingCart />} />
+              <Route path="san-pham-chi-tiet" element={<LayoutProductDetail />}>
                 <Route index element={<Products />} />
                 <Route path=":slug" element={<ProductDetail />} />
               </Route>
               <Route path="*" element={<NoPage />} />
 
               {/* Protected routes */}
-            
             </Route>
           </Routes>
         </CartProvider>
