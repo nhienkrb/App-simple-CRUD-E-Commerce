1bca9b2cc40ad446cde4e0e1f8440e57e2501886
nhiendev <nhiennkrb@gmail.com>
2025-07-26 15:30:54 +0700

    fix: error-useLocation() may be used only in the context of a <Router> component.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,18 @@
 import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AuthProvider from "../../frontend/src/context/AuthContext";
-import { CartProvider } from "../../frontend/src/context/CartContext";
-import ProtectedRoute from "../../frontend/src/routes/ProtectedRoute";
+import {  Route, Routes } from "react-router-dom";
+import AuthProvider from "../../admin/src/context/AuthContext";
+import ProtectedRoute from "../../admin/src/routes/ProtectedRoute";
 import AdminRoutes from "./routes/AdminRoutes";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <Routes>
-            {/* Công khai */}
-              <Route element={<ProtectedRoute />}>
-                <Route path="admin/*" element={<AdminRoutes />} />
-              </Route>
-          </Routes>
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+        <Routes>
+          {/* Công khai */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="admin/*" element={<AdminRoutes />} />
+          </Route>
+        </Routes>
+    </AuthProvider>
   );
 }
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { CssBaseline } from '@mui/material'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <CssBaseline />
+    <BrowserRouter>
+      <CssBaseline />
       <App />
-  </StrictMode>,
-)
+    </BrowserRouter>
+  </StrictMode>
+);
