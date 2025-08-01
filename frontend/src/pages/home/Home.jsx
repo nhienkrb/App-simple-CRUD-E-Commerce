2cb8cb1d671c8c6a3a18aa94fabf4e519f44958a
nhiendev <nhiennkrb@gmail.com>
2025-08-01 20:29:37 +0700

    feat: call api Product Recommendation

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HeroBanner from "./HeroBanner";
 import ProductList from "./ProductList";
 import Promotions from "./Promotions";
 import SlideProduct from "./SlideProduct";
 import ProductCatalog from "./ProductCatalog";
+import RecommendProduct from "./RecommendProduct";
+import { useAuth } from "../../context/AuthContext";
+import { useCart } from "../../context/CartContext";
 export default function Home() {
+  const { token } = useAuth();
+  const { cartItems } = useCart();
+
+  const shouldShowRecommend = token || (cartItems && cartItems.length > 0);
   return (
     <div>
       <HeroBanner />
       <ProductCatalog />
+      {shouldShowRecommend && <RecommendProduct />}
       <ProductList />
       <Promotions />
-    
       <SlideProduct />
     </div>
   );
---
+++ frontend/src/pages/home/RecommendProduct.jsx
@@ -0,0 +1,62 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+import useFetchList from "../../hooksCustom/useFetchList";
+import ProductCard from "../../components/ProductCard";
+import LineCut from "../../components/LineCut";
+import {useAuth} from "../../context/AuthContext";
+import { useCart } from "../../context/CartContext";
+import { useEffect, useState } from "react";
+const API_URL = `${import.meta.env.VITE_API_URL}/recommend-products`;
+export default function RecommendProduct() {
+  const { token } = useAuth();
+  const { cartItems } = useCart();
+  const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  const fetchRecommended = async () => {
+    try {
+      const res = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          ...(token && { Authorization: `Bearer ${token}` }),
+        },
+        body: JSON.stringify({
+          cart_product_ids: cartItems.map((item) => item.id),
+        }),
+      });
+
+      const data = await res.json();
+      setProducts(data.products || []);
+    } catch (error) {
+      console.error("Lỗi gợi ý sản phẩm:", error);
+    }
+  };
+
+  fetchRecommended();
+}, [token, cartItems]);
+
+
+  return (
+    <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+      <LineCut nameLine="SẢN PHẨM DÀNH CHO BẠN" />
+
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          justifyContent: {
+            xs: "center", // căn giữa trên mobile
+            sm: "flex-start", // căn trái từ màn hình sm trở lên
+          },
+        }}
+      >
+        {products.map((product) => (
+          <Grid size={{ xs: 6, sm: 4, md: 2.4 }} key={product.id}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+}
