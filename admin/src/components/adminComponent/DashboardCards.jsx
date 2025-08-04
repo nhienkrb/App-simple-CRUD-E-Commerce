f3b67d94cbff2a9a729d360a22948ef06abbc73a
nhiendev <nhiennkrb@gmail.com>
2025-08-05 00:06:53 +0700

    feat: call api to process dashboard data

--- admin/src/components/adminComponent/DashboardCards.jsx
+++ admin/src/components/adminComponent/DashboardCards.jsx
@@ -1,46 +1,110 @@
 import { Grid, Box, Typography } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
 import CampaignIcon from "@mui/icons-material/Campaign";
-import HubIcon from "@mui/icons-material/Hub";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { LineChart, PieChart } from "@mui/x-charts";
+import useFetchList from "../../hooksCustom/useFetchList";
+const API_URL = import.meta.env.VITE_API_URL;
 
-const cardData = [
-  {
-    icon: <PersonIcon sx={{ fontSize: 40, color: "#ff9800" }} />,
-    title: "Users",
-    value: "1000",
-    subtitle: "Update Now",
-    link: "/dashboard",
-  },
-  {
-    icon: <CampaignIcon sx={{ fontSize: 40, color: "#8bc34a" }} />,
-    title: "Revenue",
-    value: "$ 1,345",
-    subtitle: "Last day",
-    link: "/orders",
-  },
-  {
-    icon: <HubIcon sx={{ fontSize: 40, color: "#f44336" }} />,
-    title: "Errors",
-    value: "23",
-    subtitle: "In the last hour",
-    link: "/reports",
-  },
-  {
-    icon: <FavoriteBorderIcon sx={{ fontSize: 40, color: "#2196f3" }} />,
-    title: "Followers",
-    value: "+45K",
-    subtitle: "Update now",
-    link: "/integrations",
-  },
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
-
-
 export default function DashboardCards({ router }) {
+  const { data = [] } = useFetchList(API_URL + "/dashboard");
+  const dashboard = data[0] || {};
+  const { data: dataTop3Products = [] } = useFetchList(
+    API_URL + "/dashboard/top3-products"
+  );
+
+  const pieChart = dataTop3Products.map((product, index) => {
+    return {
+      id: index,
+      value: product.total_sold,
+      label:
+        product.product_name.length > 12
+          ? product.product_name.slice(0, 12) + "..."
+          : product.product_name,
+    };
+  });
+
+  // Fetching top 3 products sold by month
+  const { data: topProductsMonthly = [] } = useFetchList(
+    API_URL + "/dashboard/top3-products/month"
+  );
+
+  const months = [...new Set(topProductsMonthly.map((item) => item.thang))];
+  const productsSet = [
+    ...new Set(topProductsMonthly.map((item) => item.product_name)),
+  ];
+  const monthIndex = Object.fromEntries(months.map((m, i) => [m, i]));
+
+  // Tạo dữ liệu trống cho từng sản phẩm
+  const productSeries = productsSet.map((name) => ({
+    label: name.length > 12 ? name.slice(0, 12) + "..." : name,
+    data: new Array(months.length).fill(0),
+  }));
+
+  // Điền số lượng bán vào data
+  topProductsMonthly.forEach(({ thang, product_name, total_quantity }) => {
+    const series = productSeries.find((s) =>
+      s.label.includes(product_name.slice(0, 12))
+    );
+    if (series) {
+      series.data[monthIndex[thang]] += Number(total_quantity);
+    }
+  });
+
+  const cardData = [
+    {
+      icon: (
+        <ShoppingCartCheckoutIcon sx={{ fontSize: 40, color: "#ff9800" }} />
+      ),
+      title: "Total Orders Today",
+      value: dashboard.order_day || "loading...",
+      subtitle: "Today",
+      link: "/dashboard",
+    },
+    {
+      icon: <CampaignIcon sx={{ fontSize: 40, color: "#8bc34a" }} />,
+      title: "Revenue",
+      value: dashboard.revenue_today || "loading...",
+      subtitle: "Today",
+      link: "/orders",
+    },
+    {
+      icon: <PersonIcon sx={{ fontSize: 40, color: "#f44336" }} />,
+      title: "New Customers",
+      value: dashboard.new_customer || "loading...",
+      subtitle: "This Month",
+      link: "/reports",
+    },
+    {
+      icon: <InventoryIcon sx={{ fontSize: 40, color: "#2196f3" }} />,
+      title: "Products in Stock",
+      value: dashboard.product_almost_out || "loading...",
+      subtitle: "Update now",
+      link: "/",
+    },
+  ];
   return (
     <Grid container spacing={2}>
       {cardData.map((item, index) => (
-        <Grid key={index} size={{xs:12, sm:6, md:3, lg:3}}>
+        <Grid key={index} size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
           <Box
             onClick={() => router.navigate(item.link)}
             sx={{
@@ -81,7 +145,97 @@ export default function DashboardCards({ router }) {
           </Box>
         </Grid>
       ))}
+
+      <Grid size={{ xs: 12, sm: 12, md: 8, lg: 8 }}>
+        <Box
+          sx={{
+            p: 1,
+            borderRadius: 2,
+            boxShadow: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+            height: "100%",
+            bgcolor: "white",
+            cursor: "pointer",
+          }}
+        >
+          <BarChart
+            height={300}
+            series={productSeries}
+            xAxis={[{ data: months }]}
+            yAxis={[{ width: 50 }]}
+            margin={{ top: 20, right: 20, bottom: 30, left: 60 }}
+          />
+
+          <Typography
+            variant="caption"
+            color="text.secondary"
+            textAlign={"center"}
+          >
+            Top Products sold by Month
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid size={{ xs: 12, sm: 12, md: 4, lg: 4 }}>
+        <Box
+          sx={{
+            p: 1,
+            borderRadius: 2,
+            boxShadow: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+            height: "100%",
+            bgcolor: "white",
+            cursor: "pointer",
+          }}
+        >
+          <PieChart
+            series={[
+              {
+                data: pieChart,
+              },
+            ]}
+            width={400}
+            height={400}
+          />
+          <Typography
+            variant="caption"
+            color="text.secondary"
+            textAlign={"center"}
+          >
+            Top 3 Products Sold
+          </Typography>
+        </Box>
+      </Grid>
+
+      <Grid size={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
+        <Box
+          sx={{
+            p: 2,
+            borderRadius: 2,
+            boxShadow: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 1,
+            height: "100%",
+            bgcolor: "white",
+            cursor: "pointer",
+          }}
+        >
+          <LineChart
+            height={300}
+            series={[
+              { data: pData, label: "pv" },
+              { data: uData, label: "uv" },
+            ]}
+            xAxis={[{ scaleType: "point", data: xLabels }]}
+            yAxis={[{ width: 50 }]}
+          />
+        </Box>
+      </Grid>
     </Grid>
   );
 }
-
