dbc20a16b229139299712e7b438d50e6f70bf7a2
nhiendev <nhiennkrb@gmail.com>
2025-09-17 00:48:05 +0700

    fix: edit linecut ui

--- frontend/src/components/LineCut.jsx
+++ frontend/src/components/LineCut.jsx
@@ -1,14 +1,44 @@
-import { Box, Typography } from "@mui/material"
-import React from "react"
+import { Box, Typography } from "@mui/material";
 
-export default function LineCut({nameLine, fontSize = "h3"}) {
+export default function LineCut({ nameLine, fontSize }) {
   return (
-     <Box display="flex" alignItems="center" mb={2}>
-        <Box flex={1} height="1px" bgcolor="gray" />
-        <Typography variant={fontSize} fontWeight="bold" sx={{ mx: 2 }}>
+    <Box display="flex" alignItems="center" mb={2}>
+      {/* dòng đỏ trái */}
+      <Box flex={1} height="1px" bgcolor="#8c181e" />
+
+      {/* container ảnh + chữ */}
+      <Box
+        mx={2}
+        sx={{
+          position: "relative",
+          width: 300,           // điều chỉnh chiều rộng theo ảnh thật
+          height: 47,           // điều chỉnh chiều cao theo ảnh thật
+          backgroundImage: "url(/img/bg-contact1.png)",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+        <Typography
+          fontWeight="bold"
+          fontFamily={"UTM Seagull"}
+          color="#8c181e"        // hoặc màu phù hợp với nền ảnh
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            whiteSpace: "nowrap",
+            padding: "6px 20px",
+            fontSize: fontSize
+          }}
+        >
           {nameLine}
         </Typography>
-        <Box flex={1} height="1px" bgcolor="gray" />
       </Box>
-  )
-};
+
+      {/* dòng xám phải */}
+      <Box flex={1} height="1px" bgcolor="#8c181e" />
+    </Box>
+  );
+}
