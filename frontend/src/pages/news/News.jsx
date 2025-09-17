2c25007becad4bdcc0601a13e92d2fe5e0b2f466
nhiendev <nhiennkrb@gmail.com>
2025-09-17 14:05:23 +0700

    ui: edit text color

--- frontend/src/pages/news/News.jsx
+++ frontend/src/pages/news/News.jsx
@@ -1,33 +1,25 @@
-import {
-  Container,
-  Grid,
-  Typography,
-} from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 
 import BreadcrumbNav from "../../components/BreadcrumbNav";
 import PostsLatest from "../../components/newsComonent/PostsLatest";
 import PostsNews from "../../components/newsComonent/PostsNews";
+import LineCut from "../../components/LineCut";
 
 export default function News() {
-
   return (
     <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
-    
-    <BreadcrumbNav />
+      <BreadcrumbNav />
 
-      <Typography
-        fontWeight={600}
-        variant="h4"
-        gutterBottom
-        sx={{ textAlign: "center", mb: 4 }}
-      >
-        News Page
-      </Typography>
+      <LineCut
+        nameLine={"Tin Tức Đề Cập"}
+        fontSize={"1.5rem"}
+        bg_nen={"/img/bg-buton.png"}
+        colorNameLine={"#FFF"}
+      />
 
       <Grid container spacing={1}>
-          <PostsNews />
-      <PostsLatest />
-    
+        <PostsNews />
+        <PostsLatest />
       </Grid>
     </Container>
   );
